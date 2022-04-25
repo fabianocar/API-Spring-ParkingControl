852bf9c996980c6356e550e27c57e492529d926f
Fabianocar <sl.biano28@gmail.com>
2022-04-25 14:24:54 -0300

    Update controller post

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,8 +1,11 @@
 package com.api.parkingcontrol.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 
 @Service
@@ -11,5 +14,10 @@ public class ParkingSpotService {
 	
 	@Autowired
 	private ParkingSpotRepository parkingSpotRepository;
+	
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
 	
 }
