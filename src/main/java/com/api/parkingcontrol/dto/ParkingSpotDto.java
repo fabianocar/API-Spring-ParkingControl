c1f13600e311031999a2ec73344c416d32e60800
Fabianocar <sl.biano28@gmail.com>
2022-04-25 13:41:09 -0300

    Created packages dto service controller

---
+++ src/main/java/com/api/parkingcontrol/dto/ParkingSpotDto.java
@@ -0,0 +1,32 @@
+package com.api.parkingcontrol.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class ParkingSpotDto {
+
+	@NotBlank
+	private String parkingSpotNumber;
+	
+	@NotBlank
+	@Size(max = 7)
+	private String licensePlateCar;
+	
+	@NotBlank
+	private String brandCar;
+	
+	@NotBlank
+	private String modelCar;
+	
+	@NotBlank
+	private String colorCar;
+	
+	@NotBlank
+	private String responsableName;
+	
+	@NotBlank
+	private String apartment;
+	
+	@NotBlank
+	private String block;
+}
